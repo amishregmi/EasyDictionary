ac9366772809f8f6deff85525cdfb8ede614d207
[amish] <aregmi@ramapo.edu>
2019-02-02 22:17:59 -0500

    Start & stop for audio

---
+++ sdkdemo/app/src/main/java/com/microsoft/cognitiveservices/speech/samples/sdkdemo/MainActivity.java
@@ -0,0 +1,242 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+package com.microsoft.cognitiveservices.speech.samples.sdkdemo;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+import com.microsoft.cognitiveservices.speech.ResultReason;
+import com.microsoft.cognitiveservices.speech.intent.LanguageUnderstandingModel;
+import com.microsoft.cognitiveservices.speech.SpeechConfig;
+import com.microsoft.cognitiveservices.speech.intent.IntentRecognitionResult;
+import com.microsoft.cognitiveservices.speech.intent.IntentRecognizer;
+import com.microsoft.cognitiveservices.speech.SpeechRecognitionResult;
+import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
+import com.microsoft.cognitiveservices.speech.samples.sdkdemo.MicrophoneStream;
+import com.microsoft.cognitiveservices.speech.CancellationDetails;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static android.Manifest.permission.INTERNET;
+import static android.Manifest.permission.RECORD_AUDIO;
+
+public class MainActivity extends AppCompatActivity {
+
+    //
+    // Configuration for speech recognition
+    //
+
+    // Replace below with your own subscription key
+    private static final String SpeechSubscriptionKey = "f2a689a79c8840a18e4f8acaa75eacfb";
+    // Replace below with your own service region (e.g., "westus").
+    private static final String SpeechRegion = "westus";
+
+    //
+    // Configuration for intent recognition
+    //
+
+
+    private TextView recognizedTextView;
+
+
+    private Button recognizeContinuousButton;
+
+    private MicrophoneStream microphoneStream;
+
+    private MicrophoneStream createMicrophoneStream() {
+        if (microphoneStream != null) {
+            microphoneStream.close();
+            microphoneStream = null;
+        }
+
+        microphoneStream = new MicrophoneStream();
+        return microphoneStream;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recognizedTextView = findViewById(R.id.recognizedText);
+
+        recognizeContinuousButton = findViewById(R.id.buttonRecognizeContinuous);
+
+        // Initialize SpeechSDK and request required permissions.
+        try {
+            // a unique number within the application to allow
+            // correlating permission request responses with the request.
+            int permissionRequestId = 5;
+
+            // Request permissions needed for speech recognition
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, INTERNET}, permissionRequestId);
+        }
+        catch(Exception ex) {
+            Log.e("SpeechSDK", "could not init sdk, " + ex.toString());
+            recognizedTextView.setText("Could not initialize: " + ex.toString());
+        }
+
+        // create config 
+        final SpeechConfig speechConfig;
+        try {
+            speechConfig = SpeechConfig.fromSubscription(SpeechSubscriptionKey, SpeechRegion);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            displayException(ex);
+            return;
+        }
+
+        ///////////////////////////////////////////////////
+        ///////////////////////////////////////////////////
+        // recognize continuously
+        ///////////////////////////////////////////////////
+        recognizeContinuousButton.setOnClickListener(new View.OnClickListener() {
+            private static final String logTag = "reco 3";
+            private boolean continuousListeningStarted = false;
+            private SpeechRecognizer reco = null;
+            private AudioConfig audioInput = null;
+            private String buttonText = "";
+            private ArrayList<String> content = new ArrayList<>();
+
+            @Override
+            public void onClick(final View view) {
+                final Button clickedButton = (Button) view;
+                disableButtons();
+                if (continuousListeningStarted) {
+                    if (reco != null) {
+                        final Future<Void> task = reco.stopContinuousRecognitionAsync();
+                        setOnTaskCompletedListener(task, result -> {
+                            Log.i(logTag, "Continuous recognition stopped.");
+                            MainActivity.this.runOnUiThread(() -> {
+                                clickedButton.setText(buttonText);
+                            });
+                            enableButtons();
+                            continuousListeningStarted = false;
+                        });
+                    } else {
+                        continuousListeningStarted = false;
+                    }
+
+                    return;
+                }
+
+                clearTextBox();
+
+                try {
+                    content.clear();
+                    
+                    // audioInput = AudioConfig.fromDefaultMicrophoneInput();
+                    audioInput = AudioConfig.fromStreamInput(createMicrophoneStream());
+                    reco = new SpeechRecognizer(speechConfig, audioInput);
+
+                    reco.recognizing.addEventListener((o, speechRecognitionResultEventArgs) -> {
+                        final String s = speechRecognitionResultEventArgs.getResult().getText();
+                        Log.i(logTag, "Intermediate result received: " + s);
+                        content.add(s);
+                        setRecognizedText(TextUtils.join(" ", content));
+                        content.remove(content.size() - 1);
+                    });
+
+                    reco.recognized.addEventListener((o, speechRecognitionResultEventArgs) -> {
+                        final String s = speechRecognitionResultEventArgs.getResult().getText();
+                        Log.i(logTag, "Final result received: " + s);
+                        content.add(s);
+                        setRecognizedText(TextUtils.join(" ", content));
+                    });
+
+                    final Future<Void> task = reco.startContinuousRecognitionAsync();
+                    setOnTaskCompletedListener(task, result -> {
+                        continuousListeningStarted = true;
+                        MainActivity.this.runOnUiThread(() -> {
+                            buttonText = clickedButton.getText().toString();
+                            clickedButton.setText("Stop");
+                            clickedButton.setEnabled(true);
+                        });
+                    });
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                    displayException(ex);
+                }
+            }
+        });
+
+        final HashMap<String, String> intentIdMap = new HashMap<>();
+        intentIdMap.put("1", "play music");
+        intentIdMap.put("2", "stop");
+
+        ///////////////////////////////////////////////////
+        // recognize intent
+        ///////////////////////////////////////////////////
+
+    }
+
+    private void displayException(Exception ex) {
+        recognizedTextView.setText(ex.getMessage() + System.lineSeparator() + TextUtils.join(System.lineSeparator(), ex.getStackTrace()));
+    }
+
+    private void clearTextBox() {
+        AppendTextLine("", true);
+    }
+
+    private void setRecognizedText(final String s) {
+        AppendTextLine(s, true);
+    }
+
+    private void AppendTextLine(final String s, final Boolean erase) {
+        MainActivity.this.runOnUiThread(() -> {
+            if (erase) {
+                recognizedTextView.setText(s);
+            } else {
+                String txt = recognizedTextView.getText().toString();
+                recognizedTextView.setText(txt + System.lineSeparator() + s);
+            }
+        });
+    }
+
+    private void disableButtons() {
+        MainActivity.this.runOnUiThread(() -> {
+
+            recognizeContinuousButton.setEnabled(false);
+
+        });
+    }
+
+    private void enableButtons() {
+        MainActivity.this.runOnUiThread(() -> {
+
+            recognizeContinuousButton.setEnabled(true);
+
+        });
+    }
+
+    private <T> void setOnTaskCompletedListener(Future<T> task, OnTaskCompletedListener<T> listener) {
+        s_executorService.submit(() -> {
+            T result = task.get();
+            listener.onCompleted(result);
+            return null;
+        });
+    }
+
+    private interface OnTaskCompletedListener<T> {
+        void onCompleted(T taskResult);
+    }
+
+    private static ExecutorService s_executorService;
+    static {
+        s_executorService = Executors.newCachedThreadPool();
+    }
+}
